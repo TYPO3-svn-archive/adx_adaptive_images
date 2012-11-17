48ab658492b88f654ac52c2c2553bc716481b915
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-17 16:29:26 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/adx_adaptive_images/trunk@67899 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 // add XCLASS to image cObject
-$TYPO3_CONF_VARS['FE']['XCLASS']['tslib/content/class.tslib_content_image.php'] = t3lib_extMgm::extPath($_EXTKEY).'Classes/XClass/class.ux_tslib_cobj.php';
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['tslib/content/class.tslib_content_image.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClass/class.ux_tslib_cobj.php';
 
 ?>
