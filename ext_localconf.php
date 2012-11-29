9cbe46709771860a26918ea58bc68f6455f65b29
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-29 15:47:25 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/adx_adaptive_images/trunk@68090 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,10 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 // add XCLASS to image cObject
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['tslib/content/class.tslib_content_image.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClass/class.ux_tslib_cobj.php';
+// Register stdWrap hook for includeFrontEndResources. Using "adx*" key to prevent colliding of other adx-extensions using the same hook.
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap']['adxIncludeFrontEndResources'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/StdWrap.php:&Tx_AdxAdaptiveImages_Hooks_StdWrap';
+
+// load user function
+include_once(t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Service/UserFunc.php');
 
 ?>
